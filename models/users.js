e2c2badee7dbb3922e8bb649923d9033204fef5e
biezhenyu <biezhenyu521@163.com>
2018-09-25 21:45:55 +0800

    登录功能实现

--- models/users.js
+++ models/users.js
@@ -4,5 +4,13 @@ module.exports = {
   // 注册一个用户
   create: (user) => {
     return User.create(user).exec()
+  },
+
+  // 通过用户名获取用户信息
+  getUserByName: (name) => {
+    return User
+      .findOne({name: name})   // 查找某一条数据
+      .addCreatedAt()   // addCreatedAt 自定义插件（通过 _id 生成时间戳）
+      .exec()
   }
 }
